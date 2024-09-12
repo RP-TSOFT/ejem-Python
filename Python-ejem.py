2744b3989745de5beea8b085515df52d4c667fca
RP-TSOFT <Ramiro.Poggiese@tsoftglobal.com>
2024-09-12 17:36:51 -0300

    Se añade archivo Python de ejemplo

---
+++ Python-ejem.py
@@ -0,0 +1,21 @@
+# Código en Python más complejo
+
+# Definir una lista de números
+numeros = [8, 2, 15, 7, 4, 23, 5]
+
+# Imprimir la lista original
+print("Lista original:", numeros)
+
+# Ordenar la lista de menor a mayor
+numeros_ordenados = sorted(numeros)
+
+# Mostrar la lista ordenada
+print("Lista ordenada:", numeros_ordenados)
+
+# Seleccionar el número menor y el mayor
+menor = numeros_ordenados[0]
+mayor = numeros_ordenados[-1]
+
+# Mostrar el número menor y el mayor
+print("El número menor es:", menor)
+print("El número mayor es:", mayor)
